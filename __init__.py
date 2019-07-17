775af2515c8783c91f0a5049955319d9ff619a0c
stabiloBOSS96 <Lukas.ziefle@web.de>
2019-07-17 11:49:01 +0200

    Added greeting

--- __init__.py
+++ __init__.py
@@ -9,6 +9,28 @@ cola_button = Button(config.buttons["button3"])
 jackycola_button = Button(config.buttons["button2"])
 Lamp().blink()
 
+print("""/
+ ____       _       _          ___        __  __       _   
+|  _ \ _ __(_)_ __ | | __     / _ \      |  \/  | __ _| |_ 
+| | | | '__| | '_ \| |/ /____| | | |_____| |\/| |/ _` | __|
+| |_| | |  | | | | |   <_____| |_| |_____| |  | | (_| | |_ 
+|____/|_|  |_|_| |_|_|\_\     \___/      |_|  |_|\__,_|\__|
+                                                           
+                                _           _   _           
+ _ __  _ __ ___  ___  ___ _ __ | |_ ___  __| | | |__  _   _ 
+| '_ \| '__/ _ \/ __|/ _ \ '_ \| __/ _ \/ _` | | '_ \| | | |
+| |_) | | |  __/\__ \  __/ | | | ||  __/ (_| | | |_) | |_| |
+| .__/|_|  \___||___/\___|_| |_|\__\___|\__,_| |_.__/ \__, |
+|_|                                                   |___/ 
+   _  _ __        ______ _           _   _ _                                                             
+ _| || |\ \      / / ___(_)_ __ ___ | | | | |__  _ __ ___ _ __  _ __ ___  _   _ ___  ___ _   _ _ __ ___  
+|_  ..  _\ \ /\ / / |  _| | '_ ` _ \| | | | '_ \| '__/ _ \ '_ \| '_ ` _ \| | | / __|/ _ \ | | | '_ ` _ \ 
+|_      _|\ V  V /| |_| | | | | | | | |_| | | | | | |  __/ | | | | | | | | |_| \__ \  __/ |_| | | | | | |
+  |_||_|   \_/\_/  \____|_|_| |_| |_|\___/|_| |_|_|  \___|_| |_|_| |_| |_|\__,_|___/\___|\__,_|_| |_| |_|
+                                                                                                         
+
+""")
+
 while True:
     Lamp().on()
     if pfeffi_button.read_value() == 1:
@@ -17,14 +39,14 @@ while True:
         print("Pfeffi processed...")
         Barkeeper.mix_drink("pfeffi")
         print("Here you are")
-            
+
     if cola_button.read_value() == 1:
         Lamp().off()
         print("Please wait...")
         print("Cola processed...")
         Barkeeper.mix_drink("cola")
         print("Here you are")
-    
+
     if jackycola_button.read_value() == 1:
         Lamp().off()
         print("Please wait...")
@@ -34,4 +56,3 @@ while True:
 
 
 #Barkeeper.clean()
-        
